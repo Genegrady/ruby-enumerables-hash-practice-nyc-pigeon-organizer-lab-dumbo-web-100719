351c703e5c2730a4450b1697f5f8b3af67d94804
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-30 17:21:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
 def nyc_pigeon_organizer(data)
-  pigeons_list = {}
+  pigeon_list = {}
   data.each do |color_gender_lives, value|
     value.each do |stats, all_names|
       all_names.each do |name|
